9d47c68401449ea021865d2c91dda1279d879510
Tim Lundstedt <luti22bp@student.ju.se>
2025-03-08 11:00:25 +0100

    fix: adding /modules into git and update git_auto_update script

--- git_auto_update.sh
+++ git_auto_update.sh
@@ -5,7 +5,7 @@ status=$( git pull )
 status="1$(echo $status | grep -o "Remote")"
 
 echo "$status"
-if [ $status == "1" ]; then
+if [ $status == "1Remote" ]; then
   echo "Directory is now updated"
   sudo systemctl restart magic_mirror.service
 fi

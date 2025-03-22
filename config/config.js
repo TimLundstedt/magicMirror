6fe290a02629e5933fa3e58be37a261f8e0bb20c
Tim Lundstedt <luti22bp@student.ju.se>
2025-03-22 16:45:35 +0100

    update

--- config/config.js
+++ config/config.js
@@ -27,8 +27,8 @@ let config = {
 	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
 	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
 
-	language: "en",
-	locale: "en-US",   // this variable is provided as a consistent location
+	language: "sv",
+	locale: "sv-SE",   // this variable is provided as a consistent location
 			   // it is currently only used by 3rd party modules. no MagicMirror code uses this value
 			   // as we have no usage, we  have no constraints on what this field holds
 			   // see https://en.wikipedia.org/wiki/Locale_(computer_software) for the possibilities
@@ -39,10 +39,6 @@ let config = {
 
 	modules: [
 		{
-			module: 'MMM-Facts',
-			position: 'bottom_bar',
-		},
-		{
 			module: "MMM-ISS-Live",
 			header: "ISS Live Stream", // With header
 			position: "top_left", // Best in left region
@@ -50,6 +46,89 @@ let config = {
 			  url: "https://www.youtube.com/embed/xRPjKQtRXR8?si=zi-AnQaKiuwKkaLR", // Another video stream
 			}
 		},
+		{
+			module: "calendar",
+			config: 
+			{
+			  broadcastPastEvents: true, // <= IMPORTANT to see past events
+			  maximumEntries: 1000,
+			  calendars: 
+				  [
+				{
+					url: "webcal://p31-caldav.icloud.com/published/2/MTE5NzAxMjQ5NTgxMTk3MPdv8ulqWKhSKow8nWWCW0OkcZtiYsxi3MLp19VYAXB6u7GfkIKRaYUWh3G7xrTEv8Kk7WXCpX85WWDsgEER4r4",
+					name: "Icloud-Kalender", // <= RECOMMENDED to assign name
+					color: "green" // <= RECOMMENDED to assign color
+				},
+				{
+					url: "https://cal.bokatvattid.se/NEQ1MjdGMzI2OTc4UjU",
+					name: "Laundry", // <= RECOMMENDED to assign name
+					color: "blue" // <= RECOMMENDED to assign color
+				},
+				{
+					url: "webcal://p179-caldav.icloud.com/published/2/MTE5NzAxMjQ5NTgxMTk3MPdv8ulqWKhSKow8nWWCW0NDFwZQ2Bs8dDWQmYO2mbYo",
+					name: "Icloud-Arbete",
+					color: "green"
+				},
+				{
+					url: "webcal://p179-caldav.icloud.com/published/2/MTE5NzAxMjQ5NTgxMTk3MPdv8ulqWKhSKow8nWWCW0OAhg_XM63b0OA3HW3hWOEg",
+					name: "Icloud-Hem",
+					color: "white"
+				},
+				{
+					url: "https://outlook.office365.com/owa/calendar/d765ba208a7540729c8445329981a27e@student.ju.se/3fa95881e1964d3894b6bb27fb81ac594439776407567505601/calendar.ics",
+					name: "JTH",
+					color: "yellow"
+				}
+				]
+			}
+		},
+		{
+			module: "MMM-CalendarExt3",
+			position: "bottom_bar",
+			title: "",
+			config: {
+			  mode: "week",
+			  instanceId: "basicCalendar",
+			  maxEventLines: 5,
+			  firstDayOfWeek: 1,
+			  weekIndex: 0,
+			  weeksInView: 2,
+			  calendarSet: [],
+			}
+		},
+		{
+			module: "clock",
+			config: {
+			  // The config property is optional.
+			  // See 'Configuration options' for more information.
+			  showDate: false,
+			},
+		},
+		{
+			module: "weather",
+			position: "top_right",
+			config: {
+			  // See 'Configuration options' for more information.
+			  weatherProvider: "SMHI",
+				type: "forecast",
+				lat: 57.77352865961292,
+				lon: 14.146761753970969
+			},
+		},
+		{
+			module: 'MMM-WiFiPassword',
+			position: "bottom_left",
+			  config: {
+				//See 'Configuration options' for more information.
+				network: "FBI(top secret)", 
+				password: "Vamos123",
+				showPassword: false,
+				showAuthType: false,
+				header: "Wifi",
+				layoutVertical: true,
+				qrSize: 75,
+			  }
+		  },
 	]
 };
 
